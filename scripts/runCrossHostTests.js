80cc24967fadcb3a853415beeac44e1c4e9fb3dd
Utkarsh <utkarsh@crusher.dev>
2021-11-12 10:27:47 +0530

    Fix

--- scripts/runCrossHostTests.js
+++ scripts/runCrossHostTests.js
@@ -1,18 +1,24 @@
 const fetch = require("node-fetch");
 const url = require('url');
 
+const PROJECT_ID = process.env.PROJECT_ID || 264;
+const GITHUB_REPO_NAME = process.env.GITHUB_REPO_NAME || "crusherdev/cross-host-comparision";
+const GIT_COMMIT = process.env.GIT_COMMIT || "280500bc8d6e4a163a0b9cff7351184a4024c337";
+const CRUSHER_TOKEN = process.env.CRUSHER_TOKEN || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoyODgsInRlYW1faWQiOjIyNSwiaWF0IjoxNjM1MTE1MDQ5LCJleHAiOjE2NjY2NTEwNDl9.FvAN8P9tREKZlw2DOlXXk81xVd4Zq-p6-c3ahTjFIj0";
+
 const resolveToBackendPath = (path) => {
-  return url.resolve("https://backend.crusher.dev", path);
+  return url.resolve("http://localhost:8000", path);
 };
 
 (async () => {
-  const baseBuildId = await fetch(resolveToBackendPath(`/projects/${process.env.PROJECT_ID}/tests/actions/run`), {
+  const baseBuildId = await fetch(resolveToBackendPath(`/projects/${PROJECT_ID}/tests/actions/run`), {
     method: "POST",
     headers: {
-      'cookie': `token=${process.env.CRUSHER_TOKEN}`,
+      'Content-Type': 'application/json',
+      'cookie': `token=${CRUSHER_TOKEN}`,
     },
     body: JSON.stringify({
-      githubRepoName: process.env.GITHUB_REPO_NAME,
+      githubRepoName: GITHUB_REPO_NAME,
       disableBaseLineComparisions: true,
       host: "https://headout.com",
     })
@@ -32,7 +38,7 @@ const resolveToBackendPath = (path) => {
           method: "GET",
           headers: {
             'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
-            'cookie': `token=${process.env.CRUSHER_TOKEN}`,
+            'cookie': `token=${CRUSHER_TOKEN}`,
           },
         });
         const responseText = await response.text();
@@ -50,15 +56,16 @@ const resolveToBackendPath = (path) => {
 
   await waitTillBuildIsFinished(baseBuildId);
 
-  await fetch(resolveToBackendPath(`/projects/${process.env.PROJECT_ID}/tests/actions/run`), {
+  await fetch(resolveToBackendPath(`/projects/${PROJECT_ID}/tests/actions/run`), {
     method: "POST",
     headers: {
-      'cookie': `token=${process.env.CRUSHER_TOKEN}`,
+      'Content-Type': 'application/json',
+      'cookie': `token=${CRUSHER_TOKEN}`,
     },
     body: JSON.stringify({
-      githubRepoName: process.env.GITHUB_REPO_NAME,
+      githubRepoName: GITHUB_REPO_NAME,
       baselineJobId: baseBuildId,
-      githubCommitId: process.env.GIT_COMMIT,
+      githubCommitId: GIT_COMMIT,
       host: "https://stage-headout.com",
     })
   }).then(async (res) => {
